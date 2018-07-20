19c3dae00712598a61feb1f34e23291e4a5278b3
Jorge Turrado Ferrero <jturrado@stt-systems.com>
2018-07-20 11:07:54 +0200

    Muevo los scripts de Manage a Usuarios.Manage.js y añado para minificacion

---
+++ Akari-Net.Core/wwwroot/js/Usuarios.Manage.js
@@ -0,0 +1,109 @@
+﻿//Lanzo la llamada para obtener usuarios
+function GetUsers(url) {
+    $.ajax({
+        url: url,
+        //data: { },
+        type: "post",
+        success: function (data) {
+            $("#content").html(data);
+            $("#btnUsers").addClass("active")
+            $("#btnRoles").removeClass("active")
+        },
+        error: function () {
+            alert("Oops, hemos tenido un problema...");
+        }
+    });
+}
+
+//Lanzo la llamada para obtener los roles
+function GetRoles(url) {
+    $.ajax({
+        url: url,
+        //data: { },
+        type: "post",
+        success: function (data) {
+            $("#content").html(data);
+            $("#btnUsers").removeClass("active")
+            $("#btnRoles").addClass("active")
+        },
+        error: function () {
+            alert("Oops, hemos tenido un problema...");
+        }
+    });
+}
+//Guarda un nuevo rol
+function saveNewRole(url, claims) {
+    if (!$("#roleName").val()) {
+        alert("Introduce un nombre para el rol");
+        return;
+    }
+    $.ajax({
+        url: url,
+        data: { roleName: $("#roleName").val() },
+        type: "post",
+        success: function (data) {
+            $("#listaRolesUpd").append(new Option($("#roleName").val(), data, false, true));
+            $("#listaRolesDel").append(new Option($("#roleName").val(), data, false, true));
+            getClaims(claims);
+        },
+        error: function () {
+            alert("Oops, hemos tenido un problema...");
+        }
+    });
+}
+//Elimina un rol
+function removeRole(url, claims) {
+    if (!$("#listaRolesDel").val()) {
+        alert("Para poder borrar un rol primero tienes que seleccionarlo");
+        return;
+    }
+    $.ajax({
+        url: url,
+        data: { roleId: $("#listaRolesDel").val() },
+        type: "post",
+        success: function (data) {
+            $(".desplegables option[value=" + $("#listaRolesDel").val() + "]").remove();
+            getClaims(claims);
+        },
+        error: function () {
+            alert("Oops, hemos tenido un problema...");
+        }
+    });
+}
+//Muestra los claims
+function getClaims(url) {
+    if (!$("#listaRolesUpd").val()) {
+        $("#claims").html("");
+        return;
+    }
+    $.ajax({
+        url: url,
+        data: { roleId: $("#listaRolesUpd").val() },
+        type: "post",
+        success: function (data) {
+            $("#claims").html(data);
+        },
+        error: function () {
+            alert("Oops, hemos tenido un problema...");
+        }
+    });
+}
+//Actualiza los cailms del rol
+function updateClaims(url, id) {
+    $.ajax({
+        url: url,
+        data: {
+            roleId: $("#roleId").val(),
+            policyId: id,
+            set: $("#" + id).is(':checked')
+        },
+        type: "post",
+        //success: function (data) {
+
+        //},
+        error: function () {
+            $("#" + id).checked = !$("#" + id).is(':checked');
+            alert("Oops, hemos tenido un problema...");
+        }
+    });
+}
