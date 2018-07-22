bf5f8b32f01e51cfd94bc523304597696d114e5c
Jorge Turrado Ferrero <jorge_turrado@hotmail.es>
2018-07-22 14:44:06 +0200

    Muevo la funcion para enviar por ajax el correo de verificvacion

--- Akari-Net.Core/wwwroot/js/site.js
+++ Akari-Net.Core/wwwroot/js/site.js
@@ -1,9 +1,27 @@
-﻿function openInNewTab(url) {
+﻿//Abre una nueva ventana
+function openInNewTab(url) {
     var win = window.open(url, '_blank');
     win.focus();
 }
-
+//Muestra los tooltip
 $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip();
 });
+//Llamada AJAX para enviar correo de verificacion desde el profile
+function sendVerificationMail(url) {
+    $.ajax({
+        url: url,
+        data: {
+            __RequestVerificationToken: $("input[name='__RequestVerificationToken']").val(),
+            Email: $("#Email").val(),
+        },
+        type: "post",
+        success: function (data) {
+            $("body").html(data);
+        },
+        error: function () {
+            alert("Oops, hemos tenido un problema...");
+        }
+    });
+}
 
