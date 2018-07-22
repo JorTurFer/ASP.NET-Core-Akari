89d5d289a97e15257703ef6ade4dabd969e6f9ba
Jorge Turrado Ferrero <jorge_turrado@hotmail.es>
2018-07-22 13:19:49 +0200

    Creo el DB context para las citas

---
+++ Akari-Net.Core/Areas/Citas/Models/Entities/CitasDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Akari_Net.Core.Areas.Citas.Models.Entities
+{
+    public class CitasDbContext : DbContext
+    {
+        public CitasDbContext(DbContextOptions<CitasDbContext> options)
+        : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+
+        public DbSet<Cita> Citas { get; set; }
+
+        public DbSet<Paciente> Pacientes { get; set; }
+    }
+}
