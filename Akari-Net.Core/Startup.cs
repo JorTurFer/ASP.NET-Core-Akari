c2d27838a8d3294e44920b9abe709ef316822de1
Jorge Turrado Ferrero <jorge_turrado@hotmail.es>
2018-07-22 14:39:45 +0200

    Borro SenderGrid del startup

--- Akari-Net.Core/Startup.cs
+++ Akari-Net.Core/Startup.cs
@@ -91,7 +91,6 @@ namespace Akari_Net.Core
             services.AddResponseCompression();
 
             services.AddSingleton<IEmailSender, EmailSender>();
-            services.Configure<AuthMessageSenderOptions>(Configuration);
             services.AddSingleton<IPoliciesManager, PoliciesManager>();
         }
 
