105e48905f50b46b04360bd2f0f0dffa7289eae9
Jorge Turrado Ferrero <jorge_turrado@hotmail.es>
2018-07-22 13:14:04 +0200

    Añado las entidades cita y paciente

---
+++ Akari-Net.Core/Areas/Citas/Models/Entities/Cita.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Akari_Net.Core.Areas.Citas.Models.Entities
+{
+    public class Cita
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int IdPaciente { get; set; }
+        [Required]
+        public DateTime Inicio { get; set; }
+        [Required]
+        public int Duracion { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
---
+++ Akari-Net.Core/Areas/Citas/Models/Entities/Paciente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Akari_Net.Core.Areas.Citas.Models.Entities
+{
+    public class Paciente
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Nombre { get; set; }
+        [Required]
+        public DateTime Nacimiento { get; set; }
+        public string Email { get; set; }
+        public string Telefono { get; set; }
+    }
+}
