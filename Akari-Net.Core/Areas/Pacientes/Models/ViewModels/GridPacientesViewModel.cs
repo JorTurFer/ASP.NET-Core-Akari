55297436cd8403c04a3cbb538c1d37226ab20ded
Jorge Turrado Ferrero <jorge_turrado@hotmail.es>
2018-07-28 02:23:57 +0200

    Añado ViewModels para poder hacer que los pacientes se muestren en grid dinamico

---
+++ Akari-Net.Core/Areas/Pacientes/Models/ViewModels/GridPacientesViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Akari_Net.Core.Areas.Pacientes.Models.ViewModels
+{
+    public class GridPacientesViewModel
+    {
+        public string Text { get; set; }
+        public string Sort { get; set; }
+        public bool Ascending { get; set; }
+        public bool InvertAscending { get => !Ascending; }
+        public int Page { get; set; }
+        public int PageSize { get; set; }
+        public int TotalPacientes { get; set; }
+        public IEnumerable<PacienteViewModel> Pacientes { get; set; }
+    }
+}
---
+++ Akari-Net.Core/Areas/Pacientes/Models/ViewModels/PacientesPageDataViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Akari_Net.Core.Areas.Pacientes.Models.ViewModels
+{
+    public class PacientesPageDataViewModel
+    {
+        public int TotalPacientes { get; set; }
+        public IEnumerable<PacienteViewModel> Pacientes { get; set; }
+    }
+}
