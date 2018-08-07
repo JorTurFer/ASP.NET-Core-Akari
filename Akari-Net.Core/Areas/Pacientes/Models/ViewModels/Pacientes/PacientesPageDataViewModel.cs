bad164d79994faf4a52b2266ef1fe95bdf22f390
Jorge Turrado Ferrero <jorge_turrado@hotmail.es>
2018-08-07 20:49:52 +0200

    Muevo los view model a su carpeta

--- Akari-Net.Core/Areas/Pacientes/Models/ViewModels/Pacientes/PacientesPageDataViewModel.cs
+++ Akari-Net.Core/Areas/Pacientes/Models/ViewModels/Pacientes/PacientesPageDataViewModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Akari_Net.Core.Areas.Pacientes.Models.ViewModels
+namespace Akari_Net.Core.Areas.Pacientes.Models.ViewModels.Pacientes
 {
     public class PacientesPageDataViewModel
     {
