d1113e390e106342267f677454fef6369d38cf61
Jorge Turrado Ferrero <jorge_turrado@hotmail.es>
2018-08-30 00:33:38 +0200

    Añado el viewmodel a usar en las vistas de paciente

---
+++ src/Akari-Net.Core/Areas/Pacientes/Models/ViewModels/Pacientes/PacienteDataViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Akari_Net.Core.Areas.Pacientes.Models.Entities;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Akari_Net.Core.Areas.Pacientes.Models.ViewModels.Pacientes
+{
+    public class PacienteDataViewModel
+    {
+        public Paciente Paciente { get; set; }
+        public List<SelectListItem> Provincias { get; set; }
+        public List<SelectListItem> Paises { get; set; }
+    }
+}
