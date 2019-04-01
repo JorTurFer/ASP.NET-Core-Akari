8ee6c5f8a94b9dbaa41114b4dfe415767121dd63
JorTurFer <jorge_turrado@hotmail.es>
2019-04-01 22:05:06 +0200

    code quality

--- src/Akari-Net.Core/Models/Entities/SMS.cs
+++ src/Akari-Net.Core/Models/Entities/SMS.cs
@@ -5,13 +5,11 @@ using System.Threading.Tasks;
 
 namespace Akari_Net.Core.Models.Entities
 {
-
     public class SMS
     {
         //Mensaje
         public string message { get; set; }
         //Desinatario
         public string msisdn { get; set; }
-
     }
 }
